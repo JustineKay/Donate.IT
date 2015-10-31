70bea4fed28d630a56aa5bed31c850abd6e53f2a
shenayoshida <sheyoshida@gmail.com>
2015-10-31 15:53:00 -0400

    picker views working

--- Donate_It/CreateListingViewController.m
+++ Donate_It/CreateListingViewController.m
@@ -9,6 +9,24 @@
 #import "CreateListingViewController.h"
 
 @interface CreateListingViewController ()
+<
+UIPickerViewDataSource,
+UIPickerViewDelegate
+>
+
+// text fields
+@property (weak, nonatomic) IBOutlet UITextField *listingCityTextLabel;
+@property (weak, nonatomic) IBOutlet UITextField *listingStateTextLabel;
+@property (weak, nonatomic) IBOutlet UITextField *listingsModelTextLabel;
+@property (weak, nonatomic) IBOutlet UITextView *listingsDescriptionTextField;
+
+// pickers
+@property (nonatomic) IBOutlet UIPickerView *listingsDeviceTypePicker;
+@property (nonatomic) NSArray *pickerDeviceData;
+
+
+// other things
+@property (nonatomic) IBOutlet UIImageView *listingImage;
 
 @end
 
@@ -16,22 +34,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.pickerDeviceData = @[ @[@"iPhone", @"Laptop", @"Tablet"], @[@"good", @"fair", @"poor"]];
+    
+    self.listingsDeviceTypePicker.dataSource = self;
+    self.listingsDeviceTypePicker.delegate = self;
+
+    
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - picker setup
+
+// The number of columns of data
+- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// The number of rows of data
+- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return self.pickerDeviceData.count;
 }
-*/
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.pickerDeviceData[component][row];
+}
+
+#pragma mark - buttons
+
+- (IBAction)cameraButtonTapped:(id)sender {
+}
+
+- (IBAction)saveButtonTapped:(id)sender {
+}
 
 @end
