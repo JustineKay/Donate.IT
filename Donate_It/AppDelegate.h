48a2b7119ad48e87785acffc305ec55c9636c0aa
Justine Gartner <GartnerJustine@gmail.com>
2015-10-31 10:29:03 -0400

    Created Project

---
+++ Donate_It/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Donate_It
+//
+//  Created by Justine Kay on 10/31/15.
+//  Copyright © 2015 Justine Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Donate_It/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Donate_It
+//
+//  Created by Justine Gartner on 10/31/15.
+//  Copyright © 2015 Justine Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
