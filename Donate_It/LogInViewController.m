2979a5d23c96b9be77bfe4a84bde4555a449b150
cdolm92 <cdolm92@gmail.com>
2015-11-01 16:14:14 -0500

    login alert view color changes

--- Donate_It/LogInViewController.m
+++ Donate_It/LogInViewController.m
@@ -84,13 +84,13 @@
     alertViewController.alertViewBackgroundColor = [UIColor colorWithRed:0.09 green:0.66 blue:.09 alpha:1.0];
     alertViewController.alertViewCornerRadius = 10.0f;
     
-    alertViewController.titleColor = [UIColor colorWithRed:0.30f green:0.30 blue:0.30f alpha:1.0f];
-    alertViewController.messageColor = [UIColor whiteColor];
+    alertViewController.titleColor = [UIColor colorWithRed:0.44f green:0.83f blue:0.60f alpha:1.0f];
+    alertViewController.messageColor = [UIColor colorWithRed:0.38f green:0.38f blue:0.38f alpha:1.0f];
     
-    alertViewController.buttonColor = [UIColor colorWithRed:0.30f green:0.30 blue:0.30f alpha:1.0f];
+    alertViewController.buttonColor = [UIColor colorWithRed:0.44f green:0.83f blue:0.60f alpha:1.0f];
     alertViewController.buttonTitleColor = [UIColor whiteColor];
     
-    alertViewController.cancelButtonColor = [UIColor colorWithRed:0.30f green:0.30 blue:0.30 alpha:1.0f];
+    alertViewController.cancelButtonColor = [UIColor lightGrayColor];
     alertViewController.cancelButtonTitleColor = [UIColor whiteColor];
     
     
