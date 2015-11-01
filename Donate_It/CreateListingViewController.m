3028e2c9055e36fcc46cf6e4fbfa057e590a4e27
cdolm92 <cdolm92@gmail.com>
2015-11-01 11:09:53 -0500

    changed alertview log message

--- Donate_It/CreateListingViewController.m
+++ Donate_It/CreateListingViewController.m
@@ -158,7 +158,7 @@ UINavigationControllerDelegate
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
     if (buttonIndex == 0) {
-        NSLog(@"Cancel Tapped.");
+        NSLog(@"Ok Tapped.");
     }
 }
 
